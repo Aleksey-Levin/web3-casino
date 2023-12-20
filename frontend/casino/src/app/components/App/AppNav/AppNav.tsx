c40cf63a6ceb3706bf990d6c57ff82ed01592c24
Mikhail Saveliev <zdarovayrodi@icloud.com>
2023-12-20 05:14:29 +0300

    feat: add link to MainPage on logo at nav

--- frontend/casino/src/app/components/App/AppNav/AppNav.tsx
+++ frontend/casino/src/app/components/App/AppNav/AppNav.tsx
@@ -10,13 +10,13 @@ export const AppNav = () => {
         <div style={{ height: 'max-content' }} className="bg-[#30333C] text-white py-2 z-50">
             <div className="container mx-auto">
                 <div className="flex flex-row justify-between">
-                    <div className="flex flex-row items-center">
+                    <a href="/" className="flex flex-row items-center">
                         <img src="src/assets/icons/chip.png" alt="Icon" className="w-[53px] h-[53px] mr-[15px]" />
                         <span className="font-semibold text-3xl">
                             <span className="bg-gradient-to-b from-white via-blue-400 to-red-400 bg-clip-text text-transparent">Z</span>aSlot
                             <span className="text-gray-400">.bet</span>
                         </span>
-                    </div>
+                    </a>
                     <div className="flex flex-row items-center rounded-[100px] bg-[#2D313D]">
                         <div className="flex flex-row items-center rounded-[100px] border-[2px] border-[#4F5563] bg-[#3B414F] px-[14px] py-[11px] gap-[3px] cursor-pointer">
                             <GameIcon />
