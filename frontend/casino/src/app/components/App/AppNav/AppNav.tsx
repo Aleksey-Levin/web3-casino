857fcfaf24ce9a635911d2ef5cf069894140d0cc
Mikhail Saveliev <zdarovayrodi@icloud.com>
2024-01-06 15:37:55 +0500

    refactor: delete unused buttons

--- frontend/casino/src/app/components/App/AppNav/AppNav.tsx
+++ frontend/casino/src/app/components/App/AppNav/AppNav.tsx
@@ -17,39 +17,25 @@ export const AppNav = () => {
                             <span className="text-gray-400">.bet</span>
                         </span>
                     </a>
-                    <div className="flex flex-row items-center rounded-[100px] bg-[#2D313D]">
-                        <div className="flex flex-row items-center rounded-[100px] border-[2px] border-[#4F5563] bg-[#3B414F] px-[14px] py-[11px] gap-[3px] cursor-pointer">
-                            <GameIcon />
-                            <span>
-                                Gaming
-                            </span>
+                    <div className="flex flex-row gap-8">
+                        <div className="rounded-[100px] bg-[#2D313D] flex flex-row items-center justify-between shadow-inset">
+                            <div className="flex flex-row gap-[3px] items-center px-[15px] py-[11px]">
+                                <CoinIcon />
+                                {/* placeholder for balance */}
+                                <span>0.000000</span>
+                            </div>
+                            <div className="flex flex-row items-center rounded-[100px] border-[2px] border-[#4F5563] bg-[#3B414F] px-[14px] py-[11px] gap-[3px] cursor-pointer">
+                                {/* todo: load from .svg file in assets */}
+                                <WalletIcon />
+                                <button>
+                                    Wallet
+                                </button>
+                            </div>
                         </div>
-                        <div className="flex flex-row items-center px-[14px] py-[11px] gap-[3px] cursor-not-allowed">
-                            <SportIcon />
-                            <span>Sport</span>
+                        <div className="flex flex-row items-center">
+                            <span>Robin F.</span>
+                            <ProfileIcon />
                         </div>
-                    </div>
-                    <div className="flex flex-row items-center rounded-[100px] bg-[#2D313D] px-[15px] py-[11px] justify-between w-[300px] cursor-not-allowed">
-                        <span className="text-[#959CAF] select-none">Search for games</span>
-                        <SearchIcon />
-                    </div>
-                    <div className="rounded-[100px] bg-[#2D313D] flex flex-row items-center justify-between shadow-inset">
-                        <div className="flex flex-row gap-[3px] items-center px-[15px] py-[11px]">
-                            <CoinIcon />
-                            {/* placeholder for balance */}
-                            <span>0.000000</span>
-                        </div>
-                        <div className="flex flex-row items-center rounded-[100px] border-[2px] border-[#4F5563] bg-[#3B414F] px-[14px] py-[11px] gap-[3px] cursor-pointer">
-                            {/* todo: load from .svg file in assets */}
-                            <WalletIcon />
-                            <button>
-                                Wallet
-                            </button>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center">
-                        <span>Robin F.</span>
-                        <ProfileIcon />
                     </div>
                 </div>
             </div>
