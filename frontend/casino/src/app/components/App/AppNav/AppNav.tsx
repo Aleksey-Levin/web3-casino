3a9af7396bd71e57fd40d23c792ede6b53340545
Aleksey_Levin <raft.took@yandex.ru>
2024-01-07 17:35:08 +0800

    fix img

--- frontend/casino/src/app/components/App/AppNav/AppNav.tsx
+++ frontend/casino/src/app/components/App/AppNav/AppNav.tsx
@@ -1,6 +1,7 @@
 import CoinIcon from "../../icons/CoinIcon";
 import ProfileIcon from "../../icons/ProfileIcon";
 import {ConnectStateButton} from "../../../web3/connect";
+import chipImg from '../../../../assets/icons/chip.png'
 
 export const AppNav = () => {
     return (
@@ -8,7 +9,7 @@ export const AppNav = () => {
             <div className="container mx-auto">
                 <div className="flex flex-row justify-between">
                     <a href="/" className="flex flex-row items-center">
-                        <img src="src/assets/icons/chip.png" alt="Icon" className="w-[53px] h-[53px] mr-[15px]" />
+                        <img src={chipImg} alt="Icon" className="w-[53px] h-[53px] mr-[15px]" />
                         <span className="font-semibold text-3xl">
                             <span className="bg-gradient-to-b from-white via-blue-400 to-red-400 bg-clip-text text-transparent">Z</span>aSlot
                             <span className="text-gray-400">.bet</span>
