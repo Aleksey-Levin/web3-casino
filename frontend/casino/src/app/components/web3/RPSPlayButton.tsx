8c919da1992fff2f83837a92c7e23863f137740f
Mikhail Saveliev <zdarovayrodi@icloud.com>
2024-01-08 19:07:32 +0500

    chore: add buttons for each game

---
+++ frontend/casino/src/app/components/web3/RPSPlayButton.tsx
@@ -0,0 +1,7 @@
+export const RPSPlayButton = () => {
+    return (
+        <button className="bg-rose-700 rounded-[100px] shadow text-white text-2xl font-bold py-5 px-10 hover:bg-rose-600">
+            Играть
+        </button>
+    )
+}
