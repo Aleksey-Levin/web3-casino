021c155273ce5e4cf0ad89c429aad0091c0350e4
shashkevichfrida <elfrida.shashkevich@gmail.com>
2024-01-08 17:42:36 +0300

    fix: transfer token

--- Exchanger/exchanger.go
+++ Exchanger/exchanger.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nspcc-dev/neo-go/pkg/interop/storage"
 	"github.com/nspcc-dev/neo-go/pkg/interop/native/gas"
 	"github.com/nspcc-dev/neo-go/pkg/interop/lib/address"
+	//"github.com/nspcc-dev/neo-go/pkg/interop/util"
 )
 
 const (
@@ -25,50 +26,76 @@ func _deploy(data interface{}, isUpdate bool) {
 	})
 
 	if len(args.zaCoinHash) != interop.Hash160Len {
-		panic("invalid hash of zaCoin contract")
+		panic("Invalid hash of zaCoin contract")
 	}
 
 	ctx := storage.GetContext()
 	storage.Put(ctx, zaCoinHashKey, args.zaCoinHash)
 }
 
-func BuyZaCoin(gasCount int) bool {
+func BuyZaCoin(count int) {
 	ctx := storage.GetContext()
-	zaCoinHash := storage.Get(ctx, zaCoinHashKey).(interop.Hash160)
 	playerOwner := runtime.GetScriptContainer().Sender
 
-	playerBalance := gas.BalanceOf(playerOwner)
+	//balanceBefore := gas.BalanceOf(runtime.GetExecutingScriptHash())
+	gasTransfer(playerOwner, count)
+	//balanceAfter := gas.BalanceOf(runtime.GetExecutingScriptHash())
 
-	if playerBalance < gasCount {
-		panic("Insufficient funds")
-	}
-
-	//contractHash := runtime.GetExecutingScriptHash()
-	transferredGas := gas.Transfer(playerOwner, walletAddress, gasCount, nil)
-
-	if !transferredGas {
-		panic("failed to transfer gas")
-	}
-	resultAmountZaCoin := gasCount * 57
-	transferredZaCoin := contract.Call(zaCoinHash, "transfer", contract.All, walletAddress, playerBalance, resultAmountZaCoin, nil).(bool)
-
-	if !transferredZaCoin {
-		panic("Failed to transfer zaCoins")
-	} else {
-		runtime.Log("Gas balance: "+ string(gas.BalanceOf(playerOwner)))
-		//runtime.Log("ZaCoin balance: " + string(contract.Call(zaCoinHash) + "balanceOf" +string(contract.ReadStates) + string(playerOwner)))
-	}
-
-	return transferredZaCoin
+	changePlayerBalance(ctx, playerOwner, count)
+	//if (balanceBefore - balanceAfter == count) {
+	//	changePlayerBalance(ctx, playerOwner, count)
+	//} else {
+	//	util.Abort()
+	//}
 }
 
-
 func OnNEP17Payment(from interop.Hash160, amount int, data any) {
+	zacoinStr := "zacoin"
 	ctx := storage.GetContext()
 	zaCoinHash := storage.Get(ctx, zaCoinHashKey).(interop.Hash160)
 
 	callingHash := runtime.GetCallingScriptHash()
-	if !callingHash.Equals(zaCoinHash) {
-		panic("Only ZC is accepted")
+	if data == zacoinStr {
+		if !callingHash.Equals(zaCoinHash) {
+			panic("only ZC is accepted")
+		}
+	} else {
+		if !callingHash.Equals(gas.Hash) {
+		panic("only GAS is accepted")
+		}
+	}
+}
+
+func gasTransfer(playerOwner interop.Hash160, gasCount int) {
+	contractHash := runtime.GetExecutingScriptHash()
+        transferredGas := gas.Transfer(playerOwner, contractHash, gasCount, nil)
+
+        if !transferredGas {
+              panic("Failed to transfer gas")
+        }
+}
+
+
+func changePlayerBalance(ctx storage.Context, playerOwner interop.Hash160, balanceChange int) {
+	zaCoinHash := storage.Get(ctx, zaCoinHashKey).(interop.Hash160)
+	playerContract := runtime.GetExecutingScriptHash()
+
+	var from, to interop.Hash160
+	var transferAmount int
+	if balanceChange > 0 {
+		// Transfer funds from contract to player owner
+		from = playerContract
+		to = playerOwner
+		transferAmount = balanceChange
+	} else {
+		// Transfer funds from player owner to contract
+		from = playerOwner
+		to = playerContract
+		transferAmount = -balanceChange // We flip sender/receiver, but keep amount positive
+	}
+
+	transferred := contract.Call(zaCoinHash, "transfer", contract.All, from, to, transferAmount, "zacoin").(bool)
+	if !transferred {
+		panic("failed to transfer zaCoins")
 	}
 }
