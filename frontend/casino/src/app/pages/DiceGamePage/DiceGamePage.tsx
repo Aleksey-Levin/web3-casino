600f72be282ad123cccb3fef9b2a83e811683eff
Mikhail Saveliev <zdarovayrodi@icloud.com>
2024-01-20 06:57:14 +0300

    feat: add responsiveness & mobile support for dices

--- frontend/casino/src/app/pages/DiceGamePage/DiceGamePage.tsx
+++ frontend/casino/src/app/pages/DiceGamePage/DiceGamePage.tsx
@@ -1,11 +1,30 @@
 import ContainerLayout from "../../utils/ContainerLayout"
 import { DicePlayButton } from "../../components/web3/DicePlayButton"
-import diceImg from '../../../assets/img/dice-img.png'
-import {useState} from "react";
-import {useStores} from "../../hooks/useStores.tsx";
-import {observer} from "mobx-react-lite";
+import { useState } from "react";
+import { useStores } from "../../hooks/useStores.tsx";
+import { observer } from "mobx-react-lite";
+import dice1 from '../../../assets/img/dice/dice1.png'
+import dice2 from '../../../assets/img/dice/dice2.png'
+import dice3 from '../../../assets/img/dice/dice3.png'
+import dice4 from '../../../assets/img/dice/dice4.png'
+import dice5 from '../../../assets/img/dice/dice5.png'
+import dice6 from '../../../assets/img/dice/dice6.png'
+import { ChevronUp } from 'lucide-react';
+
+
+const diceImages = [
+    dice1,
+    dice2,
+    dice3,
+    dice4,
+    dice5,
+    dice6
+];
 
 export const DiceGamePage = observer(() => {
+    const [selectedDiceValues, setSelectedDiceValues] = useState<number[]>([1, 1]);
+    const [isAlertOpen, setIsAlertOpen] = useState(true);
+
     const [diceValue, setDiceValue] = useState<string | undefined>()
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const { userStore } = useStores()
@@ -24,28 +43,72 @@ export const DiceGamePage = observer(() => {
         setIsLoading(true)
     }
 
+    const handleDiceClick = (index: number) => {
+        // cycle through values 1 to 6
+        const newSelectedDiceValues = [...selectedDiceValues];
+        newSelectedDiceValues[index] = (newSelectedDiceValues[index] % 6) + 1;
+        setSelectedDiceValues(newSelectedDiceValues);
+    };
+
+    const totalSum = selectedDiceValues.reduce((sum, value) => sum + value, 0);
+
+    const handleCloseAlert = () => {
+        setIsAlertOpen(false);
+    };
+
     return (
-        <ContainerLayout>
+        <ContainerLayout className="min-h-[720px]">
             <div className="flex flex-col items-center text-white">
-                <p className="text-[46px] font-bold uppercase">
+                <p className="text-2xl sm:text-4xl font-bold uppercase mb-12">
                     брось кубики онлайн!
                 </p>
+                {isAlertOpen && <div className="flex flex-row bg-slate-800 py-4 px-4 mt-12">
+                    <div className="flex flex-col">
+                        <span className="text-md sm:text-lg font-bold text-slate-300">
+                            Нажми на кубик,
+                        </span>
+                        <span className="sm:text-md font-bold text-slate-500">
+                            чтобы изменить его значение
+                        </span>
+                    </div>
+                    <ChevronUp onClick={handleCloseAlert} />
+                </div>}
                 <div className="justify-center flex pointer-events-none">
-                    <div className="absolute w-[710px] h-[456px] bg-lime-800 rounded-[187px] blur-[300px]" />
+                    <div className="absolute w-full max-w-[710px] h-[456px] bg-lime-800 rounded-[187px] blur-[300px]" />
                 </div>
-                <div className="flex flex-row min-w-full justify-center gap-10 relative mt-12">
-                    <img src={diceImg} alt="Dice" />
-                    <img src={diceImg} alt="Dice" />
+                <div className="flex flex-row w-full max-w-[700px] justify-center gap-12 relative mt-4">
+                    {selectedDiceValues.map((value, index) => (
+                        <img
+                            key={index}
+                            src={diceImages[value - 1]}
+                            alt={`Dice ${index + 1}`}
+                            onClick={() => handleDiceClick(index)}
+                            style={{ cursor: 'pointer' }}
+                            className="w-[100px] "
+                        />
+                    ))}
                 </div>
                 {/* Кнопка для бека */}
-                <DicePlayButton onLoading={onLoading} onSuccess={onSuccess} value={5} secondValue={6}/>
-                <div className="flex flex-row justify-around w-full font-semibold">
-                    <div className="bg-gray-500 rounded-[30px] py-5 px-10 text-2xl">
-                        Вы загадали: 5 и 6
+                <DicePlayButton onLoading={onLoading} onSuccess={onSuccess} value={selectedDiceValues[0]} secondValue={selectedDiceValues[1]}
+                    className="mt-12" />
+                <div className="flex flex-col gap-4 sm:flex-row justify-around w-full font-semibold mt-12">
+                    <div className="bg-gray-500 rounded-[30px] shadow text-white text-md sm:text-lg font-bold py-2 sm:py-4 px-6 sm:px-12">
+                        <p>
+                            Выбрано: {selectedDiceValues[0]} и {selectedDiceValues[1]}
+                        </p>
                     </div>
-                    {(diceValue && !isLoading) && <div className="bg-gray-800 rounded-[30px] py-5 px-10 text-2xl">
+                    <div className="bg-gray-500 rounded-[30px] shadow text-white text-md sm:text-lg font-bold py-2 sm:py-4 px-6 sm:px-12">
+                        <p>
+                            Сумма: {totalSum}
+                        </p>
+                    </div>
+                    {/* {(diceValue && !isLoading) && <div className="bg-gray-800 rounded-[30px] shadow text-white text-md sm:text-lg font-bold py-2 sm:py-4 px-6 sm:px-12">
                         Выпало число: {diceValue}
-                    </div>}
+                    </div>} */}
+                    {/* mock */}
+                    <div className="bg-gray-800 rounded-[30px] shadow text-white text-md sm:text-lg font-bold py-2 sm:py-4 px-6 sm:px-12">
+                        Выпало число: {diceValue}
+                    </div>
                 </div>
             </div>
         </ContainerLayout>
