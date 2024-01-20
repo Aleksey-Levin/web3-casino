578ce91063cabc74721e8fe25504466fe10ae8e5
Mikhail Saveliev <zdarovayrodi@icloud.com>
2024-01-20 05:42:57 +0300

    feat(refactor): add className support to ContainerLayout component

--- frontend/casino/src/app/utils/ContainerLayout.tsx
+++ frontend/casino/src/app/utils/ContainerLayout.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 type ContainerLayoutProps = {
     children: React.ReactNode;
+    className?: string;
 };
 
-const ContainerLayout: React.FC<ContainerLayoutProps> = ({ children }) => {
+const ContainerLayout: React.FC<ContainerLayoutProps> = ({ children, className }) => {
     return (
-        <div className="flex bg-[#171A21]">
-            <div className="container mx-auto relative py-6">
+        <div className={`flex bg-[#171A21] ${className}`}>
+            <div className="container mx-auto relative py-6 min-h-[100dvh]">
                 {children}
             </div>
         </div>
